e3172fdf718586338d35f10775123aa82b52c42e
Vitrixxl <vitrice91@gmail.com>
2025-11-26 22:10:27 +0100

    fix CN

--- init-ssl.sh
+++ init-ssl.sh
@@ -5,6 +5,7 @@ if [ ! -f /etc/nginx/ssl/cert.pem ]; then
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
         -keyout /etc/nginx/ssl/key.pem \
         -out /etc/nginx/ssl/cert.pem \
-        -subj "/CN=localhost/O=Vera/C=FR"
+        -subj "/CN=34.46.246.67/O=Vera/C=FR" \
+        -addext "subjectAltName=IP:34.46.246.67,DNS:localhost"
 fi
 nginx -g "daemon off;"
