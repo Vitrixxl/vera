9a10ba3b4fd42f4a25a391d38fa5a5df3b06b649
Vitrixxl <vitrice91@gmail.com>
2025-11-27 09:12:05 +0100

    api fix

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -2,4 +2,5 @@ import { treaty } from '@elysiajs/eden';
 import type { Api } from '@shared';
 import { environment } from '../environments/environment';
 
-export const api = treaty<Api>(environment.apiUrl).api;
+//@ts-expect-error
+export const api = treaty<Api>(environment.apiUrl) as ReturnType<typeof treaty<Api>>['api'];
