b03b01795614dfaa624f70508bfb9f2d49ca8045
Vitrixxl <vitrice91@gmail.com>
2025-11-27 09:32:52 +0100

    fix url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,5 +2,5 @@ const isProduction = typeof location !== 'undefined' && location.port !== '4200'
 
 export const environment = {
   production: isProduction,
-  apiUrl: isProduction ? 'https://verabien.duckdns.org/api' : 'http://localhost:3000/',
+  apiUrl: isProduction ? 'https://verabien.duckdns.org/api' : 'http://localhost:3000/api',
 };
